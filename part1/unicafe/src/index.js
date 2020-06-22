f1623ef445716c28434232373c6449ac2d5e4c73
Borsas <borsasyt@gmail.com>
2020-06-22 11:05:44 +0300

    1.8 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,24 +2,39 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [positive, setPositive] = useState(0)
+const Statistics = (props) => {
 
-  const TextHeader = ({text}) => {
-    return <h1>{text}</h1>
-  }
-  
   const TextSmall = ({text, number}) => {
     return <div> {text} {number} </div>
   }
 
   const Positive = ({text, number}) => {
     return <div> {text} {number} % </div>
+  }
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <TextSmall text={"good"} number={props.good}/>
+      <TextSmall text={"neutral"} number={props.neutral}/>
+      <TextSmall text={"bad"} number={props.bad}/>
+      <TextSmall text={"all"} number={props.all}/>
+      <TextSmall text={"average"} number={((props.good * 1 + props.neutral * 0 + props.bad * -1) / props.all)}/>
+      <Positive text={"positive"} number={(props.good / props.all * 100 )}/>
+    </div>
+  )
+
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const TextHeader = ({text}) => {
+    return <h1>{text}</h1>
   }
 
   const Button = (props) => {
@@ -47,15 +62,7 @@ const App = () => {
       <Button onClick={handleGood} text={"good"}/>
       <Button onClick={handleNeutral} text={"neutral"}/>
       <Button onClick={handleBad} text={"bad"}/>
-      <TextHeader text="statistics"/>
-
-      <TextSmall text="good" number={good}/>
-      <TextSmall text="neutral" number={neutral}/>
-      <TextSmall text="bad" number={bad}/>
-      <TextSmall text="all" number={all}/>
-      <TextSmall text="average" number={((good * 1 + neutral * 0 + bad * -1) / all)}/>
-      <Positive text="positive" number={(good / all * 100 )}/>
-
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   )
 }
