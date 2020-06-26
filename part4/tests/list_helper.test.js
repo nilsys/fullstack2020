e641f3ef695aed4fb753405b632f0a86911d5c5a
Borsas <borsasyt@gmail.com>
2020-06-26 14:49:58 +0300

    4.4-5 done

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -6,3 +6,55 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+  
+    test('when list has only one blog equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(5)
+    })
+  })
+
+  describe("Most Likes", () => {
+      blogs = [
+          {
+              title: "1",
+              author: "1",
+              likes: 10
+          },
+          {
+              title: "2",
+              author: "2",
+              likes: 1
+          },
+          {
+              title: "3",
+              author: "3",
+              likes: 5
+          },
+          {
+            title: "4",
+            author: "4",
+            likes: 13
+        }
+      ]
+    test("Get the blog with the most likes", () => {
+        const result = listHelper.favoriteBlog(blogs)
+        const favorite = {
+            title: "4",
+            author: "4",
+            likes: 13
+        }
+        expect(result).toEqual(favorite)
+    })
+  })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let likes = 0
+    blogs.forEach(element => {
+        likes = likes + element.likes
+    });
+    return likes
+}
+
+const favoriteBlog = (blogs) => {
+
+    return blogs.find(blog => {
+        return blog.likes === Math.max.apply(Math, blogs.map(blog => blog.likes))
+    })
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
