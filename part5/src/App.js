5da41537a468c7ca2993925863be996b5d407899
Borsas <borsasyt@gmail.com>
2020-07-01 16:45:30 +0300

    Refactored Login

--- part5/src/App.js
+++ part5/src/App.js
@@ -7,8 +7,6 @@ import blogService from "./services/blogs"
 const App = () => {
     const [ blogs, setBlogs ] = useState([])
     const [ user, setUser ] = useState(null)
-    const [ username, setUsername ] = useState("")
-    const [ password, setPassword ] = useState("")
     const [ message, setMessage ] = useState(null)
     const [ messageType, setMessageType] = useState("success")
 
@@ -30,25 +28,6 @@ const App = () => {
         }
     }, [])
 
-    const handleLogin = async (e) => {
-        e.preventDefault()
-        try {
-            const loginDetails = await blogService.login({
-                "username": username,
-                "password": password
-            })
-            window.localStorage.setItem(
-                "loggedInUser", JSON.stringify(loginDetails)
-            )
-            setUser(loginDetails)
-            blogService.setToken(loginDetails.token)
-            setPassword("")
-            setUsername("")
-        } catch (err){
-            changeMessage("Wrong username or password", "error")
-        }
-    }
-
     const changeMessage = (message, messageType) => {
         setMessage(message)
         setMessageType(messageType)
@@ -60,10 +39,8 @@ const App = () => {
     return (
         <div>
             {user === null ?
-                <Login username={username} password={password}
-                    setUsername={setUsername} setPassword={setPassword}
-                    handleLogin={handleLogin}
-                    message={message} messageType={messageType}/>
+                <Login message={message} messageType={messageType}
+                setUser={setUser} changeMessage={changeMessage}/>
                 :
                 <Blogs user={user} setUser={setUser}
                     blogs={blogs} setBlogs={setBlogs}
--- part5/src/components/Login.js
+++ part5/src/components/Login.js
@@ -1,19 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
 import Notification from "./Notification"
+import blogService from "../services/blogs"
+import loginService from "../services/login"
 
 const Login = (props) => {
+    const [ username, setUsername ] = useState("")
+    const [ password, setPassword ] = useState("")
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        try {
+            const loginDetails = await loginService.login({
+                "username": username,
+                "password": password
+            })
+            window.localStorage.setItem(
+                "loggedInUser", JSON.stringify(loginDetails)
+            )
+            blogService.setToken(loginDetails.token)
+            setPassword("")
+            setUsername("")
+            props.setUser(loginDetails)
+        } catch (err){
+            props.changeMessage("Wrong username or password", "error")
+        }
+    }
+
     return (
         <div>
             <h2>Log in to application</h2>
             <Notification message={props.message} type={props.messageType}/>
-            <form onSubmit={props.handleLogin}>
+            <form onSubmit={handleLogin}>
                 <div>
                     Username:
-                    <input value={props.username} onChange={({ target }) => props.setUsername(target.value)}/>
+                    <input value={username} onChange={({ target }) => setUsername(target.value)}/>
                 </div>
                 <div>
                     Password:
-                    <input type="password" value={props.password} onChange={({ target }) => props.setPassword(target.value)}/>
+                    <input type="password" value={password} onChange={({ target }) => setPassword(target.value)}/>
                 </div>
                 <button type="submit">Login</button>
             </form>
