7edab0c6f73588e13874871e5173cc552d8b5c79
Borsas <borsasyt@gmail.com>
2020-07-01 14:39:58 +0300

    5.7 done

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../index.css"
 
 const Blog = ({blog}) => {
+    const [ showBlog, setShowBlog] = useState(false)
+    const [ buttonText, setButtonText ] = useState("view")
+
+    const visibility = {display: showBlog ? " " : "none"}
+
+    const handleShowBlog = () => {
+        setShowBlog(!showBlog)
+        setButtonText(buttonText === "view" ? "cancel" : "view")
+    }
+
     return (
-    <div>
-        {blog.title} {blog.author}
-    </div>
+        <div className={"blogStyle"}>
+            <div>
+                {blog.title} <button onClick={handleShowBlog}>{buttonText}</button>
+            </div>
+            <div style={visibility}>
+                <div>
+                    {blog.url}
+                </div>
+                <div>
+                    {blog.likes}
+                    <button>Like</button>
+                </div>
+                <div>
+                    {blog.author}
+                </div>
+            </div>
+        </div>
     )
 }
 
