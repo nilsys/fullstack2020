241b6189207cd0be3b1657056683c371d8ce54ba
Borsas <borsasyt@gmail.com>
2020-07-15 13:19:11 +0300

    7.4 done

--- part7/src/components/CreateNew.js
+++ part7/src/components/CreateNew.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react"
+import { useField } from "../hooks/index"
 import { Redirect } from "react-router-dom"
 
 const CreateNew = (props) => {
-    const [ content, setContent ] = useState("")
-    const [ author, setAuthor ] = useState("")
-    const [ info, setInfo ] = useState("")
+    const content = useField("text")
+    const author = useField("text")
+    const info = useField("text")
+
     const [ redirect, changeRedirect ] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        props.changeNotification(`A new Anecdote ${content} created!`)
+        props.changeNotification(`A new Anecdote ${content.value} created!`)
         changeRedirect(true)
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
   }
@@ -26,15 +28,15 @@ const CreateNew = (props) => {
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-                    <input name="content" value={content} onChange={(e) => setContent(e.target.value)} />
+                    <input {...content} />
                 </div>
                 <div>
                     author
-                    <input name="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    <input {...author} />
                 </div>
                     <div>
                     url for more info
-                    <input name="info" value={info} onChange={(e)=> setInfo(e.target.value)} />
+                    <input {...info} />
                 </div>
                 <button>create</button>
             </form>
