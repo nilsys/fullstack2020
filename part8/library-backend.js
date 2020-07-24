d67ffbb4164826c6f5de482c2f75ff637b4292ca
Borsas <borsasyt@gmail.com>
2020-07-24 14:35:02 +0300

    8.3 done

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -91,10 +91,17 @@ const typeDefs = gql`
         genres: [String]!
     }
 
+    type Author {
+        name: String!
+        born: Int!
+        bookCount: Int!
+    }
+
     type Query {
         bookCount: Int!
         authorCount: Int!
         allBooks: [Book!]!
+        allAuthors: [Author!]!
     }
 
 `
@@ -103,7 +110,13 @@ const resolvers = {
   Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
-      allBooks: () => books
+      allBooks: () => books,
+      allAuthors: () => authors
+  },
+  Author: {
+      bookCount: (root) => {
+          return books.filter(b => b.author === root.name).length
+      }
   }
 }
 
