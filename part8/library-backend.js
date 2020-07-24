c2607e3a422634593d87798efbae78acc66efc82
Borsas <borsasyt@gmail.com>
2020-07-24 22:03:32 +0300

    8.7 done

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -112,6 +112,11 @@ const typeDefs = gql`
         published: Int!
         genres: [String]!
         ): Book
+
+        editAuthor(
+            name: String!
+            setBornTo: Int
+        ): Author
     }
 
 `
@@ -142,6 +147,13 @@ const resolvers = {
         const book = {...args, id: uuid()}
         books = books.concat(book)
         return book
+    },
+    editAuthor: (root, args) => {
+        let author = authors.find(a => a.name === args.name)
+        if (!author) return null
+        author.born = args.setBornTo
+        authors = authors.map(a => a.name === author.name ? author : a)
+        return author
     }
   }
 }
